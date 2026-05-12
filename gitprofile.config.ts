7085d0612f1c749b011f59736e04fb62ad471056
kingviperthesnake <btorres19@csustan.edu>
2026-05-12 10:16:55 -0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -37,7 +37,7 @@ const CONFIG = {
           description:
             'M.S. Thesis. Integrating ecological niche modeling and morphometric analyses to determine patterns in landscape-scale shifts in species ranges, historic refugia, and signals of ecological adaptation left on functional morphology.',
           imageUrl:
-            '',
+            'https://github.com/kingviperthesnake/kingviperthesnake.github.io/raw/main/Screenshot%202026-05-12%20101449.png',
           link: 'https://example.com',
         },
         {
